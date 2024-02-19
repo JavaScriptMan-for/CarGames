26ee2a22063d415dc8920d146a35e82dd48e751c
Pumastd <minevskiia10@gmail.com>
2024-02-19 10:41:08 +0300

    debug

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const music = new Audio();
+music.volume = 0.6;
 music.src = "src/audio/music.mp3";
 console.log("Кликните по экрану и музыка включится");
 let ry = Math.random()*100+1;
 function fi () {
     alert("Можно включить музыку, кликнув по экрану в любое место");
-    localStorage.setItem("b", true)
+    localStorage.setItem("music", true)
 }
-if(ry > 80 && localStorage.getItem("b") == 'false') {
+if(ry > 80 && localStorage.getItem("music") == 'false') {
     fi ()
 
 }
@@ -17,6 +18,9 @@ document.addEventListener('scroll', pL)
 function pL() {
     music.play()
 }
+setInterval(()=>{
+    music.play()
+},1000)
 document.querySelector('.butt').addEventListener('click', ()=>{
     document.location.href = "main.html"
 });
